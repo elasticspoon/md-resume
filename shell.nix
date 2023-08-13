df7b72b45d4075a624bb0cf802a8c1701729130d
YuriBocharov <quesadillaman@gmail.com>
2023-08-13 18:29:19 -0400

    makes pure shell work

--- shell.nix
+++ shell.nix
@@ -1,13 +1,20 @@
 with import <nixpkgs> { }; let
-  env = bundlerEnv {
-    ruby = pkgs.ruby;
-    name = "md_resume-bundler-env";
-    gemfile = ./Gemfile;
-    lockfile = ./Gemfile.lock;
-    gemset = ./gemset.nix;
-  };
+  env =
+    bundlerEnv
+      {
+        ruby = pkgs.ruby;
+        name = "markdown-resume-bundler-env";
+        gemfile = ./Gemfile;
+        lockfile = ./Gemfile.lock;
+        gemset = ./gemset.nix;
+      };
 in
 stdenv.mkDerivation {
-  name = "md_resume";
-  buildInputs = [ env ];
+  name = "markdown-resume";
+  buildInputs = with pkgs; [ ruby chromium ] ++ [ env ];
+
+  shellHook = ''
+    PATH="${env}/bin:$PATH"
+    alias dev_start='foreman start'
+  '';
 }
